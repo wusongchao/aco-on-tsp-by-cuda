d3e344f8110c2ee989e744dfdc3d747c024efaea
yutianhuihong <2256912385@qq.com>
2017-11-29 12:02:20 +0800

    "clean" the code

--- aco-on-tsp-by-cuda/kernel.cu
+++ aco-on-tsp-by-cuda/kernel.cu
@@ -37,75 +37,6 @@ __device__ void warpReduce(int tid, float in, float* data)
 
 constexpr int MAX_POINT_NUM = 256;
 
-void clearAllowed(bool* allowed, int cityNum)
-{
-	for (int i = 1;i <= cityNum;i++) {
-		allowed[i] = true;
-	}
-}
-
-int calculateProbabilityAndSelect(double Tau[][MAX_POINT_NUM], double heuristicValue[][MAX_POINT_NUM],
-	bool allowed[], int currentCityIndex, int cityNum, double alpha, double beta)
-{
-	std::map<int, double> probability;
-
-	double denominatorSum = 0;
-	for (int s = 1;s <= cityNum;s++) {
-		if (allowed[s] == true) {
-			double val = pow(Tau[currentCityIndex][s], alpha)*pow(heuristicValue[currentCityIndex][s], beta);
-			probability.insert({ s,val });
-			denominatorSum += val;
-		}
-	}
-
-	int selectedCityIndex = -1;
-	if (probability.size() == 1) {
-		//means we have no city for choosing
-		selectedCityIndex = probability.begin()->first;
-	}
-	else {
-		double sumSelect = 0;
-
-		std::default_random_engine randomEngine;
-		std::uniform_real_distribution<double> randomDistribution(0, 1);
-		double generatedProbablity = randomDistribution(randomEngine);
-
-		for (auto &r : probability) {
-			r.second = r.second / denominatorSum;
-			sumSelect += r.second;
-			if (sumSelect >= generatedProbablity) {
-				selectedCityIndex = r.first;
-				break;
-			}
-		}
-		selectedCityIndex = selectedCityIndex == -1 ? (--probability.end())->first : selectedCityIndex;
-	}
-
-	allowed[selectedCityIndex] = false;
-
-	return selectedCityIndex;
-}
-
-void calculateDeltaTauPerAnt(double deltaTau[][MAX_POINT_NUM], int Q, std::vector<int>* path, double antPathLength)
-{
-	for (int k = 0, pathLength = path->size() - 1;k < pathLength;k++) {
-		int i = path->operator[](k);
-		int j = path->operator[](k + 1);
-		deltaTau[i][j] = deltaTau[j][i] += Q / antPathLength;
-	}
-}
-
-
-void updateTau(double Tau[][MAX_POINT_NUM], double deltaTauTotal[][MAX_POINT_NUM], double rho, int cityNum)
-{
-	for (int i = 1;i <= cityNum;i++) {
-		for (int j = 1;j <= cityNum;j++) {
-			double val = (1.0 - rho) * Tau[i][j] + deltaTauTotal[i][j];
-			Tau[j][i] = Tau[i][j] = val;
-		}
-	}
-}
-
 __inline__ __device__ void clearTauMatrix(unsigned int tid, float* src)
 {
 	src[tid] = 0.0f;
@@ -151,55 +82,48 @@ __inline__ __device__ int generateInitialCityIndex(curandState* state, int cityN
 	return curand(state) % cityNum;
 }
 
-//__shared__ int path[MAX_POINT_NUM];
-//__shared__ int current;
-//__shared__ int startCityIndex;
-//__shared__ int pathSize;
-//__shared__ float currentDistance;
+__shared__ int path[MAX_POINT_NUM];
+__shared__ int current;
+__shared__ int startCityIndex;
+__shared__ float currentDistance;
+__shared__ float probabilities[MAX_POINT_NUM];
+__shared__ float denominatorSum;
+__shared__ bool allowed[MAX_POINT_NUM];
 
-//__device__ void calculateProbability(int tid, float* Tau, float* heuristicValue, int cityNum, float alpha, float beta)
-//{
-//	// each thread mapped to a city(start from zero)
-//	if (allowed[tid] == true) {
-//		const int offset = MAX_POINT_NUM;
-//		// must ensure that val not all is 0, so, have to make Tau an initial value instead of 0
-//		float val = powf(Tau[current * offset + tid], alpha)*powf(heuristicValue[current * offset + tid], beta) + 0.1f;
-//		// means possibility from current to tid
-//		probabilities[tid] = val;
-//		atomicAdd(&denominatorSum, val);
-//	}
-//}
-//
-//__device__ int selectCity(curandState* state, int cityNum)
-//{
-//	float generatedProbablity = curand_uniform(state);
-//	float sumSelect = 0.0f;
-//	int selectedIndex = 0;
-//	for (int i = 0; i < cityNum;i++) {
-//		if (allowed[i] == true) {
-//			//sumSelect += 1;
-//			sumSelect += (probabilities[i] / denominatorSum);
-//			if (sumSelect >= generatedProbablity) {
-//				selectedIndex = i;
-//				break;
-//			}
-//		}
-//	}
-//	return selectedIndex;
-//}
+
+__inline__ __device__ void calculateProbability(int tid, float* Tau, float* heuristicValue, int cityNum, float alpha, float beta)
+{
+	// each thread mapped to a city(start from zero)
+	if (allowed[tid] == true) {
+		const int offset = MAX_POINT_NUM;
+		// must ensure that val not all is 0, so, have to make Tau an initial value instead of 0
+		float val = powf(Tau[current * offset + tid], alpha)*powf(heuristicValue[current * offset + tid], beta);
+		// means possibility from current to tid
+		probabilities[tid] = val;
+		atomicAdd(&denominatorSum, val);
+	}
+}
+
+__inline__ __device__ int selectCity(curandState* state, int cityNum)
+{
+	float generatedProbablity = curand_uniform(state);
+	float sumSelect = 0.0f;
+	int selectedIndex = 0;
+	for (int i = 0; i < cityNum;i++) {
+		if (allowed[i] == true) {
+			sumSelect += (probabilities[i] / denominatorSum);
+			if (sumSelect >= generatedProbablity) {
+				selectedIndex = i;
+				break;
+			}
+		}
+	}
+	return selectedIndex;
+}
 
 __global__ void constructPath(float* Tau, float* deltaTau, float* graphMatrix, float* heuristicValue, curandState* states, int cityNum, float alpha, float beta, int Q, 
 	float* bestLength, int* bestPath)
 {
-	__shared__ int path[MAX_POINT_NUM];
-	__shared__ int current;
-	__shared__ int startCityIndex;
-	//__shared__ int pathSize;
-	__shared__ float currentDistance;
-	__shared__ float probabilities[MAX_POINT_NUM];
-	__shared__ float denominatorSum;
-	__shared__ bool allowed[MAX_POINT_NUM];
-
 	int tid = threadIdx.x;
 	int bid = blockIdx.x;
 	const int offset = MAX_POINT_NUM;
@@ -218,37 +142,11 @@ __global__ void constructPath(float* Tau, float* deltaTau, float* graphMatrix, f
 		__syncthreads();
 
 		while (innerPathSize < cityNum) {
-			////if (tid < cityNum) {
-			//	calculateProbability(tid, Tau, heuristicValue, cityNum, alpha, beta);
-			//	// must ensure that before select, the probabilities have been calculated
-			////}
-			if (allowed[tid] == true) {
-				const int offset = MAX_POINT_NUM;
-				// must ensure that val not all is 0, so, have to make Tau an initial value instead of 0
-				float val = powf(Tau[current * offset + tid], alpha)*powf(heuristicValue[current * offset + tid], beta);
-				// means possibility from current to tid
-				probabilities[tid] = val;
-				atomicAdd(&denominatorSum, val);
-			}
-
+			calculateProbability(tid, Tau, heuristicValue, cityNum, alpha, beta);
 			__syncthreads();
 
 			if (tid == 0) {
-				float generatedProbablity = curand_uniform(states + bid);
-				float sumSelect = 0.0f;
-				int selectedIndex = 0;
-				for (int i = 0; i < cityNum;i++) {
-					if (allowed[i] == true) {
-						//sumSelect += 1.0f;
-						sumSelect += (probabilities[i] / denominatorSum);
-						if (sumSelect >= generatedProbablity) {
-							selectedIndex = i;
-							break;
-						}
-					}
-				}
-				int next = selectedIndex;
-				//int next = selectCity(states + bid, cityNum);
+				int next = selectCity(states + bid, cityNum);
 				allowed[next] = false;
 				currentDistance += graphMatrix[current * offset + next];
 				path[innerPathSize] = next;
@@ -409,109 +307,3 @@ int main()
 
 	return 0;
 }
-
-//int main()
-//{
-//	std::string filePath;
-//
-//	std::cin >> filePath;
-//
-//	std::ifstream in(filePath.c_str());
-//	if (!in.is_open()) {
-//		std::cout << "No exist!" << std::endl;
-//	}
-//
-//	std::vector<Point> points;
-//	int index;
-//	double x, y;
-//	int cityNum = 0;
-//	while (in >> index >> x >> y) {
-//		points.push_back({ index,x,y });
-//	}
-//
-//	double graphMatrix[MAX_POINT_NUM][MAX_POINT_NUM];
-//	double heuristicValue[MAX_POINT_NUM][MAX_POINT_NUM];//启发式因子为两点间距离的倒数
-//
-//	for (auto &p : points) {
-//		cityNum++;
-//		for (auto &pr : points) {
-//			double val = sqrt((p.x - pr.x)*(p.x - pr.x) + (p.y - pr.y)*(p.y - pr.y));
-//			graphMatrix[p.index][pr.index] = graphMatrix[pr.index][p.index] = val;
-//			heuristicValue[p.index][pr.index] = heuristicValue[pr.index][p.index] = 1 / val;
-//		}
-//	}
-//
-//	constexpr int antNum = 32;
-//	constexpr int iterTimes = 800;
-//	double alpha = 1;//信息素的重要程度
-//	double beta = 3.8;//启发式因子的重要程度
-//	double rho = 0.7;//挥发系数
-//	int left = 2;//信息素增量
-//	int Q = 400;//信息素增加数
-//	double Tau[MAX_POINT_NUM][MAX_POINT_NUM];//全局的信息素信息
-//
-//
-//	std::vector<int> bestPath(cityNum);
-//	double bestLength = DBL_MAX;
-//
-//	for (int i = 1;i <= cityNum;i++) {
-//		for (int j = 1;j <= cityNum;j++) {
-//			Tau[i][j] = 0;
-//		}
-//	}
-//
-//	int iterCounter = 0;
-//	while (iterCounter < iterTimes) {
-//		std::default_random_engine randomEngine;
-//		std::uniform_int_distribution<int> randomDistribution(1, cityNum);
-//
-//		double deltaTauTotal[MAX_POINT_NUM][MAX_POINT_NUM];
-//		for (int i = 1;i <= cityNum;i++) {
-//			for (int j = 1;j <= cityNum;j++) {
-//				deltaTauTotal[i][j] = 0;
-//			}
-//		}
-//
-//		for (int i = 0;i < antNum;i++) {
-//			//for each ant
-//			bool allowed[MAX_POINT_NUM];
-//			std::vector<int> path;
-//			double currentDistance = 0;
-//			clearAllowed(allowed, cityNum);
-//
-//			int startNum = randomDistribution(randomEngine);
-//			path.push_back(startNum);
-//			allowed[startNum] = false;
-//
-//			int current = startNum;
-//			while (path.size() < cityNum) {
-//				int next = calculateProbabilityAndSelect(Tau, heuristicValue, allowed, current, cityNum, alpha, beta);
-//
-//				//add it to path and update distance
-//				path.push_back(next);
-//				currentDistance += graphMatrix[current][next];
-//
-//				current = next;
-//			}
-//
-//			calculateDeltaTauPerAnt(deltaTauTotal, Q, &path, currentDistance);
-//
-//			if (currentDistance < bestLength) {
-//				bestLength = currentDistance;
-//
-//				std::copy(path.cbegin(), path.cend(), bestPath.begin());
-//			}
-//		}
-//		updateTau(Tau, deltaTauTotal, rho, cityNum);
-//
-//		iterCounter++;
-//	}
-//	for (auto &p : bestPath) {
-//		std::cout << p << ' ';
-//	}
-//	std::cout << '\n' << bestLength << std::endl;
-//	getchar();
-//	getchar();
-//	return 0;
-//}
-
